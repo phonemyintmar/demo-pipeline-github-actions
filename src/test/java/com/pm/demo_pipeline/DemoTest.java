95091407abf9f961f2f0e3f734c0c3ebf5dbe745
PM Laptop <PM@home>
2024-07-06 15:06:28 +0700

    Add unit test for testing

---
+++ src/test/java/com/pm/demo_pipeline/DemoTest.java
@@ -0,0 +1,14 @@
+package com.pm.demo_pipeline;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+    @Test
+    public void test() {
+        var x = 0;
+        var y = 1;
+        Assertions.assertNotEquals(x, y);
+    }
+}
